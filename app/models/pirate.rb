57779846b48493af4d6d9855ee7e5eaf362a707e
Jonathan Ghandforoush <jonnyghan@gmail.com>
2020-04-23 00:09:14 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,15 @@
 class Pirate
+    attr_accessor :name, :weight, :height
+    @@crew = []
+    def initialize(params)
+        @name = params[name]
+        @weight = params[weight]
+        @height = params[height]
+        @@crew << self
+    end
+
+    def self.all
+       @@crew
+    end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+    attr_accessor :name, :type, :booty
+
+    @@fleet= []
+    def initialize(params)
+        @name = params[name]
+        @type = params[type]
+        @booty = params[booty]
+        @@fleet << self
+    end
+
+    def self.all
+        @@fleet
+    end
+
+    def self.clear
+        @@fleet.clear
+    end
+    
+
 end
